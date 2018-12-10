e1497c6d8efe6f3f08c762bd00ce86391a3fb84b
kumaran-14 <kumarangowrisankar@yahoo.in>
2018-12-10 17:38:19 +0530

    Prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('express')()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 const db = require('./config/keys').mongoURI
 const users = require('./routes/api/users')
@@ -23,8 +24,13 @@ app.use('/api/users', users)
 app.use('/api/posts', posts)
 app.use('/api/profile', profile)
 
-app.get('/', (req, res) => {
-  res.send('Ahoy!')
-})
+// for production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 
 app.listen(PORT, () => console.log(`Server started on PORT:${PORT}`))
